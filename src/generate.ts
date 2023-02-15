b8d76fe2db56aa2b783d3ad17a6308b8b3c7ebc9
Christopher Nethercott <ccnethercott@gmail.com>
2023-02-15 13:28:42 +0000

    refactor: into functions

--- src/generate.ts
+++ src/generate.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import {CalendarType, getAcademicYear, getCurrentAcademicYear, getNextPeriod} from "./calendar";
+import {academicYears, CalendarType, getAcademicYear, getCurrentAcademicYear, getNextPeriod} from "./calendar";
 import {ICalCalendar, ICalEventBusyStatus, ICalEventTransparency} from "ical-generator";
 import hash from "hash.js";
 import * as fs from "fs";
@@ -7,45 +7,58 @@ import * as fs from "fs";
 const today = dayjs().add(3, 'hour');
 const currentAcademicYear = getCurrentAcademicYear(today.toDate());
 
-if (!fs.existsSync('./src/public/calendar')) {
-    fs.mkdirSync('./src/public/calendar', { recursive: true });
-}
-
 function capitaliseFirstLetter(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-for (let calendarType of [CalendarType.UNDERGRADUATE, CalendarType.POSTGRADUATE, CalendarType.STAFF]) {
-    const calendar = new ICalCalendar();
-
-    let name = `University of York's Week Numbers (${capitaliseFirstLetter(calendarType)})`
-
-    calendar.name(name);
-    calendar.description(name);
-
-    for (let currentPeriod of currentAcademicYear.periods) {
-        let nextPeriod = getNextPeriod(currentPeriod, currentAcademicYear);
-
-        let startDate = dayjs(currentPeriod.startDate).add(3, 'hours');
-        let endDate = dayjs(nextPeriod.startDate).subtract(1, 'day');
-
-        let weeks = endDate.diff(startDate, 'weeks');
-
-        for (let i = 0; i <= weeks; i++) {
-            let currentDate = dayjs(startDate).add(i, 'weeks').toDate();
-
-            calendar.createEvent({
-                start: currentDate,
-                end: currentDate,
-                id: hash.sha256().update(`${currentPeriod.getFormattedString(currentDate, calendarType)}${currentDate}`).digest('hex'),
-                allDay: true,
-                busystatus: ICalEventBusyStatus.FREE,
-                transparency: ICalEventTransparency.TRANSPARENT,
-                summary: currentPeriod.getFormattedString(currentDate, calendarType)
-            })
-        }
-    }
-
-    calendar.save(`./src/public/calendar/${calendarType as string}.ics`).then().catch(err => console.error(err));
-    console.log(`Successfully generated calendar of type ${calendarType as string} for academic year ${getAcademicYear(currentAcademicYear)}`)
+// Generate Calendar Files
+if (!fs.existsSync('./src/public/calendar')) {
+    fs.mkdirSync('./src/public/calendar', { recursive: true });
 }
+
+function generateCalendarFiles() {
+    console.log('Generating calendar files...')
+
+    for (let calendarType of [CalendarType.UNDERGRADUATE, CalendarType.POSTGRADUATE, CalendarType.STAFF]) {
+        const calendar = new ICalCalendar();
+
+        let name = `University of York's Week Numbers (${capitaliseFirstLetter(calendarType)})`
+
+        calendar.name(name);
+        calendar.description(name);
+
+        for (let currentPeriod of currentAcademicYear.periods) {
+            let nextPeriod = getNextPeriod(currentPeriod, currentAcademicYear);
+
+            let startDate = dayjs(currentPeriod.startDate).add(3, 'hours');
+            let endDate = dayjs(nextPeriod.startDate).subtract(1, 'day');
+
+            let weeks = endDate.diff(startDate, 'weeks');
+
+            for (let i = 0; i <= weeks; i++) {
+                let currentDate = dayjs(startDate).add(i, 'weeks').toDate();
+
+                calendar.createEvent({
+                    start: currentDate,
+                    end: currentDate,
+                    id: hash.sha256().update(`${currentPeriod.getFormattedString(currentDate, calendarType)}${currentDate}`).digest('hex'),
+                    allDay: true,
+                    busystatus: ICalEventBusyStatus.FREE,
+                    transparency: ICalEventTransparency.TRANSPARENT,
+                    summary: currentPeriod.getFormattedString(currentDate, calendarType)
+                })
+            }
+        }
+
+        calendar.save(`./src/public/calendar/${calendarType as string}.ics`).then().catch(err => console.error(err));
+        console.log(`Successfully generated calendar of type ${calendarType as string} for academic year ${getAcademicYear(currentAcademicYear)}`)
+    }
+}
+
+// Generate Dates JSON File
+function generateDateFile() {
+    console.log('Generating dates json file...')
+}
+
+generateCalendarFiles();
+generateDateFile();
